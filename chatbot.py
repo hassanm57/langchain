6b37b47d60b592076dd184c26abdec1a3b2a4899
hassanm57 <hassanmansoor1569@gmail.com>
2026-07-21 21:08:10 +0500

    added a jupyter notebook consisting of langchain exercises solved

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 load_dotenv()
-model = ChatOpenAI(model_name="gpt-4", temperature=0.8)
+model = ChatOpenAI(model_name="gpt-4", temperature=0, max_tokens=500)
 prompt = "Hello! How can I assist you today? What would you like to know or discuss?"
 
 response = model.invoke(prompt)
